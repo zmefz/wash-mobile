3687070f9f9623b7c1ededde253199e13f5b325f
Saigak Evgeniy <saigakj@inbox.ru>
2018-06-10 11:18:35 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+
+export default class App extends Component<Props> {
+  render() {
+    return false
+  }
+}
