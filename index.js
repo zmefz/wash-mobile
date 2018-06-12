62e7c3bd0f43c5ca0d2b62aba8d8e62b477e0d85
Saigak Evgeniy <saigakj@inbox.ru>
2018-06-12 12:33:51 +0300

    add ignore for react-native warning

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import { AppRegistry } from 'react-native'
+import { YellowBox } from 'react-native'
 import App from './src/App'
+
+// DEVNOTE: ignore warning from react-native
+// https://github.com/react-navigation/react-navigation/issues/3956
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader',
+])
 
 AppRegistry.registerComponent('TimeToWash', () => App)
