2d52eacf01cf670ccd9168f1ae1434e80eb898a8
Saigak Evgeniy <saigakj@inbox.ru>
2018-06-12 16:49:39 +0300

    add toggle mode button at main page

--- src/containers/Main/MainPage.js
+++ src/containers/Main/MainPage.js
@@ -1,17 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'react-native'
 import { connect } from 'react-redux'
 import MainPageView from './MainPageView'
 
-// TODO: add @connect(store => ...)
 export default class MainPage extends React.Component {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }) => ({
     title: 'Main',
-  }
+    headerRight: navigation.getParam('modeButton'),
+  })
 
   static propTypes = {}
 
+  state = {
+    editing: false,
+  }
+
+  componentDidMount() {
+    this.renderModeButton()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.editing !== this.state.editing) {
+      this.renderModeButton()
+    }
+  }
+
+  renderModeButton() {
+    const { editing } = this.state
+    const title = editing ? 'Back' : 'Edit'
+    const action = this.toggleMode
+
+    this.props.navigation.setParams({
+      modeButton: <Button title={title} onPress={action} />,
+    })
+  }
+
+  toggleMode = () => {
+    this.setState(({ editing }) => ({ editing: !editing }))
+  }
+
   render() {
-    return <MainPageView />
+    const { editing } = this.state
+    return <MainPageView editing={editing} />
   }
 }
--- src/containers/Main/MainPageView.js
+++ src/containers/Main/MainPageView.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Text, View } from 'react-native'
+import { Text, ScrollView } from 'react-native'
 
 export default class MainPageView extends React.Component {
   static propTypes = {}
 
   render() {
+    const { editing } = this.props
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Main</Text>
-      </View>
+      <ScrollView>
+        <Text>Main{editing ? ' (editing)' : ''}</Text>
+      </ScrollView>
     )
   }
 }
